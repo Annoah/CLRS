b19e5401076bca1f7f97534b2be0ef5bfd26ed63
Annoah <qian_yi_ning@foxmail.com>
2018-07-14 11:53:02 +0800

    fix 2.3-6

--- Chapter 2/2.3-6.cpp
+++ Chapter 2/2.3-6.cpp
@@ -18,7 +18,6 @@ int Binary_Search(int A[], int p, int r, int v){
 	}else{
 		return p;
 	}
-	//虽然定位是O(lgn)
 }
 
 int main(){
@@ -34,7 +33,11 @@ int main(){
 			int key = a[j]; 
 			int k = j - 1;
 			int l = Binary_Search(a, 0, k, key);
-			printf("locate:%d ",l);
+			//第一步定位以及定位在已排序队列末尾时出现问题，该if结构解决该问题 
+			if((k == 0 && a[k] < key) || (l == k && a[k] < key)){
+				continue;
+			}
+			//虽然定位是O(lgn)
 			//但是定位之后还是要移动该位置之后的所有元素，因此还是O(lgn^2) = O(n)
 			while(k >= l){
 				a[k + 1] = a[k];
